4599530f9167766d531b03317ac734f5cb4e6862
aaqaishtyaq <aaqaishtyaq@gmail.com>
2017-04-15 06:19:01 +0530

    ASCII TEXT ART ADDED

--- client-module/linux.sh
+++ client-module/linux.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+echo $"
+
+
+
+
+
+ _______  _______           _______  _______         _______ _________
+(  ____ )(  ___  )|\     /|(  ____ \(  ____ )       (  ____ )\__   __/
+| (    )|| (   ) || )   ( || (    \/| (    )|       | (    )|   ) (
+| (____)|| |   | || | _ | || (__    | (____)| _____ | (____)|   | |
+|  _____)| |   | || |( )| ||  __)   |     __)(_____)|  _____)   | |
+| (      | |   | || || || || (      | (\ (          | (         | |
+| )      | (___) || () () || (____/\| ) \ \__       | )      ___) (___
+|/       (_______)(_______)(_______/|/   \__/       |/       \_______/
+
+
+
+
+
+
+
+"
 sudo apt-get install python3-venv
 python3 -m venv power-pi
 source power-pi/bin/activate
 pip install --upgrade pip
 pip install pymysql
-python linux.py
+nohup python linux.py &
 deactivate
 #etc
-
